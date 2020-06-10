9a8ebc8ed617c6a61a93d1fe4b514bc08fea54b2
anhardeni <anhardeni@gmail.com>
2020-06-10 18:37:53 +0700

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -12,7 +12,7 @@ let body = `
 
     <h1 class="center-text">
       <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
+      Hello world! deno practice  by work from home - Continous Learning
     </h1>
 
     <p class="center-text">
